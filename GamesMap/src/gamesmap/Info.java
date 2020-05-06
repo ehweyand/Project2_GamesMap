fe55913c3747894daee04c729320aa2375703cb3
Anekai <36900069+Anekai@users.noreply.github.com>
2020-05-05 21:41:29 -0300

    Update Info.java

--- GamesMap/src/gamesmap/Info.java
+++ GamesMap/src/gamesmap/Info.java
@@ -11,4 +11,70 @@ package gamesmap;
  */
 public class Info {
     
+    private String name;
+    private String platform;
+    private String rating;
+    private BigDecimal score;
+    private String genre;
+    private String recommended;
+    private Integer yearOfRelease;
+
+    public Info() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+
+    public void setScore(BigDecimal score) {
+        this.score = score;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getRecommended() {
+        return recommended;
+    }
+
+    public void setRecommended(String recommended) {
+        this.recommended = recommended;
+    }
+
+    public Integer getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(Integer yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+    
 }
