fd653f30c79af020671192047a0175b865d5eee0
evand <evand@DESKTOP-R0GMO40>
2020-05-05 21:24:48 -0300

    Creating Project - first commit

---
+++ GamesMap/src/gamesmap/SimpleReader.java
@@ -0,0 +1,37 @@
+package gamesmap;
+import java.io.*;
+
+public class SimpleReader {
+    private BufferedReader bufferedReader;
+
+    public SimpleReader(String filename) {
+        try {
+            FileReader fileReader = new FileReader(filename);
+            bufferedReader = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file not found: " + filename);
+        }
+    }
+
+    public String readLine() {
+        try {
+            if (bufferedReader != null) {
+                return bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file");
+        }
+        return null;
+    }
+
+    public void close() {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        } catch(IOException e) {
+            System.err.println("Error closing file");
+        }
+        bufferedReader = null;
+    }
+}
